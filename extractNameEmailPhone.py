5d11be4ac8e3e4668e294803c30bedd266a9a0f4
yusuffachruddin <yusuf.fachruddin@gmail.com>
2019-05-24 00:19:10 +0700

    extract Name

---
+++ extractNameEmailPhone.py
@@ -0,0 +1,59 @@
+# The typical information extraction architecture works as follows:
+
+# Segment the body — split the text into an array of sentences
+# Tokenize — split each sentence into an array of words
+# Part of Speech Tagging (POS) — tag each word with a grammatical label
+# Chunking — group and label multi-token sequences
+
+import nltk
+import re
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+stop = stopwords.words('english')
+
+string = """ Hey,
+This week has been crazy. Attached is my report on IBM. Can you give it a quick read and provide some feedback.
+Also, make sure you reach out to Will Smith (claire@xyz.com).
+You're the best.
+
+212-555-1234
+"""
+
+def extract_phone_numbers(string):
+    r = re.compile(r'(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})')
+    phone_numbers = r.findall(string)
+    return [re.sub(r'\D', '', number) for number in phone_numbers]
+
+def extract_email_addresses(string):
+    r = re.compile(r'[\w\.-]+@[\w\.-]+')
+    return r.findall(string)
+
+def ie_preprocess(document):
+    document = ' '.join([i for i in document.split() if i not in stop])
+    sentences = nltk.sent_tokenize(document)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    sentences = [nltk.pos_tag(sent) for sent in sentences]
+    return sentences
+
+def extract_names(document):
+    names = []
+    sentences = ie_preprocess(document)
+    for tagged_sentence in sentences:
+        for chunk in nltk.ne_chunk(tagged_sentence):
+            if type(chunk) == nltk.tree.Tree:
+                if chunk.label() == 'PERSON':
+                    names.append(' '.join([c[0] for c in chunk]))
+    return names
+
+if __name__ == '__main__':
+    numbers = extract_phone_numbers(string)
+    emails = extract_email_addresses(string)
+    names = extract_names(string)
+
+print(numbers)
+emails
+names
