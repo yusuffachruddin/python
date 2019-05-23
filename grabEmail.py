b7fad177b3e4840f2cf2ccede3440e1869d4d2cf
Home <yusuf.fachruddin@gmail.com>
2019-05-23 23:29:09 +0700

    grab email commit

--- grabEmail.py
+++ grabEmail.py
@@ -1,1 +1,10 @@
-sad
+import re
+
+def getEmail(text):
+    emails = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", text)
+    return emails
+
+
+text = "Please contact us at contact@tutorialspoint.com for further information. You can also give feedbacl at feedback@tp.com"
+
+print(getEmail(text))
